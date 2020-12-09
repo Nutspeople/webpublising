5c0ea1d86828f4d119381db5235654619c13f06c
Nutspeople <nutspeople@gmail.com>
2020-12-09 13:02:45 +0900

    2020-12-09 popup

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,11 +1,13 @@
 (function($) {    
     $(document).ready(function() {
+        // title toggle
         $('.tit .btn').click(function(e) {
             e.preventDefault();
             $('#cont_nav').slideToggle(200);
             $(this).toggleClass('on');
         }); 
         
+        // banner slick
         $('.ban').slick({
             infinite: true,
             slidesToShow: 3,
@@ -26,5 +28,52 @@
               $this.next('ul').show().parent('li').addClass('active').siblings('li').removeClass('active').find('>ul').hide();
           }
           $tab_list.find('ul>li>a').click(tabMenu).focus(tabMenu); 
+
+          // gallery slick
+          $('.gallery_img').slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay:true,
+            autoplaySpeed:3000,
+            pauseOnHover:true,
+            fade:true,
+            arrows:false
+          });
+          // Slick gallery play btn setting
+          $('.play').click(function() {
+            $('.gallery_img').slick('slickPlay'); 
+          });
+          $('.pause').click(function() {
+            $('.gallery_img').slick('slickPause'); 
+          });
+          $('.prev').click(function() {
+            $('.gallery_img').slick('slickPrev'); 
+          });
+          $('.next').click(function() {
+            $('.gallery_img').slick('slickNext'); 
+          });
+
+          // layer
+          $('.layer').click(function(e) {
+              e.preventDefault();
+              $('#layer').fadeIn();
+          })
+
+          // Layer popup
+          $('#layer .close').click(function(e){
+            e.preventDefault();
+            $('#layer').fadeOut();
+          }); 
+
+        // Window popup
+        $('.window').click(function(e) {
+            e.preventDefault();
+            window.open('popup.html','popup','width=800,height=590,left=50,top=50,scrollbars=0,toolbar=0,menubar=0');
+        });
+
+        // Light gallery
+        $(".lightgallery").lightGallery();
+
     });
 })(jQuery); 
