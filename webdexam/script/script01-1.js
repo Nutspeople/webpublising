bec553b6b4b46c9d958e56e9486d8dc36b88cc1f
Nutspeople <nutspeople@gmail.com>
2021-01-06 17:45:12 +0900

    webdexam and megabox site

---
+++ webdexam/script/script01-1.js
@@ -0,0 +1,9 @@
+(function($) { 
+    $('.nav > ul > li').mouseenter(function() {
+        $(".nav > ul > li > ul").stop().slideDown(200); 
+    });
+    
+    $('.nav > ul > li').mouseleave(function() {
+        $(".nav > ul > li > ul").stop().slideUp(200); 
+    });
+})(jQuery);
