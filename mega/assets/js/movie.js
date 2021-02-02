a20f4fec9e62ce27bd99a9dc11761fe0b2080248
Nutspeople <nutspeople@gmail.com>
2021-02-02 12:45:34 +0900

    youtube api

--- mega/assets/js/movie.js
+++ mega/assets/js/movie.js
@@ -3,70 +3,100 @@
     /* 트레일러 영상 플레이어를 활성화 */
     /* YouTube iframe API: https://developers.google.com/youtube/player_parameters */
     
-    (function handleTrailer() {
+    (function handleTrailer(){
+        
+        var tag = document.createElement('script');
+
+        tag.src = "https://www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
         // 셀렉터 캐시 
         var $selector = {
-            body : $('body'),
-            overlay : $('#blackout'),
-            modal : $('#trailerModal'),
-            showButton : $("#showTrailer"),
-            hideButton : $("#hideTrailer")
-        }; 
+            body: $( "body" ),
+            overlay: $( "#blackout" ),
+            modal: $( "#trailerModal" ),
+            showButton: $( "#showTrailer" ),
+            hideButton: $( "#hideTrailer" ),
+        };
         
-        // 플레이어 
+        // 플레이어
         var player = {
-            obj : null, // 플레이어 오브젝트 
+            obj: null, // 플레이어 오브젝트
             query : {
-                theme :"dark",
-                color : "white", 
-                controls : 1, 
-                autoplay : 1, 
-                enablejsapi : 1, 
-                modestbranding: 0,  // Youtube 로고 감춤
-                rel : 0,    // 관련 동영상 표시
-                showinfo : 0,   // 제목, 업로더 감춤
-                iv_load_policy : 3  // 특수효과 감춤   
+                theme: "dark",
+                color: "white",
+                controls: 1, 
+                autoplay: 1,
+                enablejsapi: 1,
+                modestbranding: 0, // YouTube 로고 감춤
+                rel: 0,  // 관련 동영상 표시
+                showinfo: 0, // 제목, 업로더 감춤
+                iv_load_policy: 3 // 특수효과 감춤
             },
-            visible : false
+            visible: false
         };
         
         // 보이기, 숨기기 버튼 활성화 
         $selector.showButton.on('click', showPlayer);
         $selector.hideButton.on('click', hidePlayer);
         
-        // Youtube Api 를 이용해 iframe 생성 
-        function setPlayer(id) {
-            player = new YT.Player('trailer', {
-               height: '480',
-               width: '282',
-               videoId: '1J3m1jJCj88',
-               playerVars : player.query
-             });
+        //YouTube API를 이용해 iframe을 생성
+        function setPlayer( id ){
+            player.obj = new YT.Player( "trailer", {
+                width: 480,
+                height: 282,
+                videoId: id,    
+                playerVars: player.query
+            });
             
-            // 처음 플레이어 크기 설정 
+            // 처음 플레이어 크기 설정
             resizePlayer();
             
-            // 리사이즈 화면 회전시 플레이어 크기 다시 설정 
-            $( window ).on('resize orientationchange', function() {
-                
+            // 리사이즈 화면 회전시 플레이어 크기 다시 설정
+            $( window ).on( "resize orientationchange", function() {
+                resizePlayer();
             });
         }
 
-        function showPlayer () {
+        // iframe 보이기
+        function showPlayer() {
+            if ( !player.obj ) {
+                setPlayer( $selector.showButton.data("youtube") );
+            }
             
-        }; 
+            $selector.body.addClass("modal_on");
+            $selector.overlay.show();
+            player.visible = true;
+        };
         
-        function hidePlayer () {
+        // iframe 감추기
+        function hidePlayer (){
+            player.obj.stopVideo();
+            $selector.overlay.hide();
+            $selector.body.removeClass("modal_on");
+            player.visible = false;
+        };
+        
+        function resizePlayer(){
+          var viewport = {}, 
+              frame = {}, 
+              modal = {}; 
             
-        }; 
-        
-        function resizePlayer() {
+            // 현재 viewport 구하기
+            viewport.width = $(window).width(); 
+            viewport.height = $(window).height(); 
             
+            // 새로운 frame 사이즈 계산 하기 
+            frame.width = viewport.width;
+            frame.height = frame.width / 1.6; // 16:10 
+            
+            // modal 위치 값 지정해주기 
+            modal.top = ((viewport.height - frame.height)/2)+"px";
+            modal.left = "0px";
+            
+            $selector.modal.css(modal); 
+            player.obj.setSize( frame.width, frame.height );
         }; 
-        
-        
-        
-    }); 
-   
-
+    })(); 
 })(jQuery);     
